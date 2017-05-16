68047e3d27da1376a9bcd8644f416c9b21c67eaa
Neil Campbell <neil@codecise.com>
2017-05-17 00:29:15 +1000

    Adding sessions listing

---
+++ content/view-sessions.md
@@ -0,0 +1,6 @@
+---
+title: Sessions
+type: sessions
+---
+Below are the sessions that have been submitted.
+Please not that we manually approve all entries so don't worry if you have just submitted a session and it is not appearing yet.
---
+++ scripts/generate-sessions-data.js
@@ -0,0 +1,88 @@
+var azure = require('azure-storage');
+var tableSvc = azure.createTableService();
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+
+var abbreviatePresenterName = function(name) {
+  if(name == null || name === '') {
+    return '';
+  }
+
+  var nameSplit = name.split(',');
+  var names = [];
+  for(var i = 0; i < nameSplit.length; i++) {
+  	var name = nameSplit[i].trim().replace(/(\b[a-zA-Z])[a-zA-Z]* /, '$1. ');
+    names.push(name);
+  }
+
+  return names.join(', ').trim();
+};
+
+var normaliseWebsite = function(url) {
+  if(url == null || url === '') {
+    return null;
+  }
+
+  if(!url.startsWith('http://') && !url.startsWith('https://')) {
+    return 'http://'+url;
+  }
+
+  return url;
+};
+
+var normaliseTwitterHandle = function(twitterHandle) {
+  if(twitterHandle == null || twitterHandle === '') {
+    return null;
+  }
+
+  return twitterHandle.replace('@', '');
+};
+
+var getApprovedSessions = function(callback) {
+  var sessions = [];
+  var query = new azure.TableQuery()
+    .where('Status eq ?', 1);
+
+  tableSvc.queryEntities('Sessions', query, null, function(err, result, response) {
+    if(err != null) {
+      console.error('Error finding sessions', err);
+      callback(sessions);
+      return;
+    }
+
+    if(!result.entries || result.entries.length == 0) {
+      console.log('No sessions submitted yet');
+      callback(sessions);
+      return;
+    }
+
+    var entries = _.orderBy(result.entries, [function(entry) { return entry.Timestamp._.toISOString(); }], ['desc'])
+    for(var i = 0; i < entries.length; i++) {
+      var entry = entries[i];
+      var session = {
+        id: entry.RowKey._,
+        title: entry.SessionTitle._,
+        name: abbreviatePresenterName(entry.PresenterName._),
+        twitter: normaliseTwitterHandle(entry.PresenterTwitterAlias._),
+        website: normaliseWebsite(entry.PresenterWebsite._),
+        abstract: entry.SessionAbstract._
+      };
+      sessions.push(session);
+    }
+
+    callback(sessions);
+  });
+};
+
+function writeSessionsAsData(sessions) {
+  var filePath = path.join(__dirname, '..', 'data', 'sessions.json');
+  fs.writeFile(filePath, JSON.stringify(sessions, null, 2), 'utf8', function(err) {
+    if(err != null) {
+        return console.error('Error writing sessions file', err);
+    }
+    console.log('Sessions written to ' + filePath);
+});
+}
+
+getApprovedSessions(writeSessionsAsData);
