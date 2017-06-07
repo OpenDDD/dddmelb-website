bb5cb166a9da2efc93f939a1dc9b2adb4c6b6750
Neil Campbell <neil@codecise.com>
2017-06-07 22:16:30 +1000

    Adding less and js build pipelines

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
 var path = require('path');
 
 gulp.task("less", function () {
-  console.log('Rebuilding styles');
-
-  return gulp.src('src/less/**/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
+  console.log('Rebuilding Styles');
+  return gulp.src('src/less/screen.less')
+    .pipe(less())
+    .pipe(concat('screen.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('static/css'));
 });
 
-gulp.task("watch", ["less"], function () {
+gulp.task("js", function () {
+  console.log('Rebuilding JS');
+  return gulp.src('src/js/**/*.js')
+    .pipe(uglify({ie8: true}))
+    .pipe(concat('site.js'))
+    .pipe(gulp.dest('static/js'));
+});
+
+gulp.task("watch", ["less", "js"], function () {
   gulp.watch("src/less/**/*.less", ["less"]);
+  gulp.watch("src/js/**/*.js", ["js"]);
 });
---
+++ src/js/site.js
@@ -0,0 +1,176 @@
+$(document).ready(function() {
+
+  updateScreenSize = function() {
+    getWinWidth = function() {
+      windowWidth = $(window).width();
+    };
+    getWinWidth();
+
+    $(window).resize(function() {
+        getWinWidth();
+    });
+    }();
+
+
+  mobileNav = function() {
+    var $navPull = $('nav.main .pull');
+    var $mainNav = $('nav.main ul');
+
+    $navPull.unbind().bind('click', function(){
+      $mainNav.toggleClass('active');
+      $(this).toggleClass('active');
+    });
+
+  }();
+
+
+  targetBlank = function() {
+      $('a.targetBlank').click(function() {
+        window.open(this.href);
+        return false;
+      });
+      $('.targetBlank a').click(function() {
+        window.open(this.href);
+        return false;
+      });
+    }();
+
+
+  accordion = function() {
+    $('.toggleAccordion').click(function(e) {
+      e.preventDefault();
+      var $this = $(this);
+
+      if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.removeClass('active');
+      }
+      else {
+        $this.parent().parent().find('.inner').removeClass('show');
+        $('.toggleAccordion').removeClass('active');
+        $this.next().addClass('show');
+        $this.addClass('active');
+      }
+    });
+
+    // Open first accordion on load
+    $('.toggleAccordion').eq(0).addClass('active');
+    $('.toggleAccordion').eq(0).next().addClass('show');
+
+
+  }();
+
+
+  smoothScroll = function(){
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+          } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+          };
+        });
+        }
+      }
+    });
+  }();
+
+
+  equalHeights = function(){
+    adjustHeight = function() {
+      // Please add the class .equal-heights to the columns container and .col to each column:
+
+      if(windowWidth > 768) {
+        $('.equal-heights').each(function(){
+
+
+          $('.col', this).height('');
+
+
+          var highestBox = 0;
+          $('.col', this).each(function(){
+            if($(this).height() > highestBox) {
+              highestBox = $(this).height();
+            }
+          });
+          $('.col',this).height(highestBox);
+
+
+
+         });
+      }
+      else {
+        $('.equal-heights').each(function(){
+          $('.col').height('');
+        });
+      }
+    }
+    adjustHeight();
+
+    $(window).resize(function() {
+            setTimeout(adjustHeight(), 100);
+        });
+
+  }();
+
+
+  eventDoneIcon = function(){
+    $('.imp-date').each(function(){
+      if($(this).hasClass('done')) {
+        $(this).prepend('<span class="ico-done"></span>');
+      }
+    });
+  }();
+
+  $('.captcha-form input[type="submit"]').attr("disabled", "disabled");
+
+  /*
+  dropInModal = function(){
+    var $body = $('body');
+    var $modal = $('.modal-content');
+    var $modalLink = $('.modal-link');
+
+    if($modalLink.length) { // If required on page
+      $body.append('<div class="modal-overlay"></div>');
+      var $modalOverlay = $('.modal-overlay');
+
+      $modalLink.on('click', function(e){
+        e.preventDefault();
+        $(this).next($modal).addClass('active');
+        $modalOverlay.addClass('active');
+        $body.addClass('modal');
+      });
+    }
+
+
+  }();*/
+
+});
+
+var recaptchaComplete = function() {
+  $('.captcha-form input[type="submit"]').removeAttr("disabled");
+};
